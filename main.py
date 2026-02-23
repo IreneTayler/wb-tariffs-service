94b9ad436d8e14a1906ee43dbab7c5adda9a9277
IreneTayler <Irene19tayler@outlook.com>
2026-02-23 16:49:29 +0300

    completed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import psycopg2
 
 app = FastAPI()
 
-DATABASE_URL = "postgresql://postgres:<12345678>@localhost:5432/tariffs_db"
+DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/tariffs_db"
 
 
 def get_db_connection():
@@ -24,17 +24,20 @@ def read_root():
 
 @app.post("/create_tariff/")
 def create_tariff(tariff: Tariff):
-    conn = get_db_connection()
-    cur = conn.cursor()
-
-    cur.execute(
-        "INSERT INTO tariffs (tariff_name, price) VALUES (%s, %s)",
-        (tariff.tariff_name, tariff.price),
-    )
-    conn.commit()
-    cur.close()
-    conn.close()
-    return {"message": "Tariff created successfully"}
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        # Create a new tariff
+        cur.execute(
+            "INSERT INTO tariffs (tariff_name, price) VALUES (%s, %s)",
+            (tariff.tariff_name, tariff.price),
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+        return {"message": "Tariff created successfully"}
+    except Exception as e:
+        return {"error": str(e)}
 
 
 @app.get("/tariffs/")
